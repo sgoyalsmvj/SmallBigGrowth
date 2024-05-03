c5c56e6363401f7da630061d0bf949f29f50d75b
sgoyalsmvj <sgoyalsmvj@gmail.com>
2024-05-04 00:34:21 +0530

    cookie

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,19 +1,16 @@
 import jwt from "jsonwebtoken";
 
 const authenticateUser = (req, res, next) => {
-  // console.log(req.cookies);
   const token = req.cookies.token;
-  console.log(token);
   if (!token) {
     return res.status(401).json({ error: "Unauthorized - No token provided" });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
     if (err) {
       return res.status(401).json({ error: "Unauthorized - Invalid token" });
     }
-    req.user = user;
-    // console.log(req.body);
+    req.user = decodedToken;
     next();
   });
 };
