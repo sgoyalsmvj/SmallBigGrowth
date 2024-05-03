32f349c6a96b8dd65f46f5a3b707981418d77745
sgoyalsmvj <sgoyalsmvj@gmail.com>
2024-05-03 22:31:15 +0530

    fix model

--- server/models/user.js
+++ server/models/user.js
@@ -1,23 +1,56 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  paymentInfo: {
+    type: Object,
+    required: false,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  try {
+    if (this.isModified("password")) {
+      this.password = await bcrypt.hash(this.password, 8);
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+    return token;
+  } catch (error) {
+    // Throw the error to propagate it further
+    throw new Error("Error generating authentication token");
+  }
+};
+
+userSchema.methods.matchPassword = async function (password) {
+  try {
+    const isMatch = await bcrypt.compare(password, this.password);
+    return isMatch;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const User = mongoose.model("User", userSchema);
+
 export default User;
