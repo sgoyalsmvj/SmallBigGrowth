a344a1e927c470df8b896c58ad670e6bd97af9df
sgoyalsmvj <sgoyalsmvj@gmail.com>
2024-05-04 00:56:27 +0530

    fixes

--- server/passport.js
+++ server/passport.js
@@ -1,31 +1,24 @@
-import  GoogleStrategy from "passport-google-oauth20";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import passport from "passport";
 
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://smallbiggrowthbackend.onrender.com/google/callback",
-      scope: [ "profile", "email" ],
-    },
-    (accessToken, refreshToken, profile, callback) => {
-      console.log("passport callback function fired");
-        console.log(profile);
-        callback(null, profile);
-    }
-  )
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackURL: "/google/callback",
+			scope: ["profile", "email"],
+		},
+		function (accessToken, refreshToken, profile, callback) {
+			callback(null, profile);
+		}
+	)
 );
 
 passport.serializeUser((user, done) => {
-  console.log("serializing user");
-  done(null, user);
-}); 
-
-passport.deserializeUser((user, done) => {
-  console.log("deserializing user");
-  done(null, user);
+	done(null, user);
 });
 
-export default passport;
-
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
